c36d583ee1359850ecf0bc482eec57607b723a69
Kasper B. Graversen <kbilsted@users.noreply.github.com>
2018-10-30 22:03:43 +0100

    manual observer

--- samples/Okanshi.Sample/Program.cs
+++ samples/Okanshi.Sample/Program.cs
@@ -1,78 +1,70 @@
 ﻿using System;
-using System.Threading;
+using System.Collections.Generic;
 using System.Threading.Tasks;
-using Okanshi;
 using Newtonsoft.Json;
 
 namespace Okanshi.Sample
 {
-    // The point for this sample is to showcase the different kinds for monitors,
-    // and how they could be used.
     public class Program
     {
-        public static readonly AutoResetEvent closing = new AutoResetEvent(false);
-        public static IMetricPoller poller;
-        public static IProcessingMetricObserver observer;
-
         public static void Main(string[] args)
         {
-            // Get the default registry instance. This is the registry used when adding metrics
-            // through OkanshiMonitor.
             var registry = DefaultMonitorRegistry.Instance;
+            new MyObserver(new MetricMonitorRegistryPoller(registry, TimeSpan.FromSeconds(2), false));
 
-            // Create a new poller to get data from the registry every 10 seconds
-            poller = new MetricMonitorRegistryPoller(registry, TimeSpan.FromSeconds(10), false);
-
-            // Add a memory observer getting data from the poller, and keep the last 100 observations
-            // in memory
-            observer = new MemoryMetricObserver(poller, 100);
-
-            // Start the actual program
-            Start();
-
-            // This flushes all the measurements to the observer, before displaying the result
-            poller.PollMetrics().Wait();
-
-            // Write the result as JSON
-            Console.WriteLine("Measurements:");
-            var observations = observer.GetObservations();
-            var result = JsonConvert.SerializeObject(observations, Formatting.Indented);
-            Console.WriteLine(result);
-        }
-
-        public static void Start() {
-            Console.WriteLine("Enter 10 numbers");
-            var count = 0;
-            while (count < 10) {
-                // Time how long the user takes to enter input.
-                var value = OkanshiMonitor.Timer("TimeToEnterInput").Record(() => {
-                    Console.Write("Enter an integer: ");
-                    return Console.ReadLine();
-                });
-
-                int result;
-                if (!Int32.TryParse(value, out result)) {
-                    Console.WriteLine("Invalid number, try again...");
-                    // Count the number of invalids numbers, using a tag to indicate that the
-                    // number was invalid
-                    OkanshiMonitor.Counter("Numbers", new[] { new Tag("state", "Invalid") }).Increment();
-                    continue;
+            Console.WriteLine("Monitor key presses. Start typing to measure");
+            while (true)
+            {
+                var info = Console.ReadKey();
+                string measurementName;
+                if ((int) info.Modifiers == 0)
+                {
+                    measurementName = info.Key.ToString();
+                }
+                else
+                {
+                    measurementName = $"{info.Modifiers} + {info.Key}";
                 }
 
-                // Count the number of valids numbers, using a tag to indicate that the
-                // number was valid
-                OkanshiMonitor.Counter("Numbers", new[] { new Tag("state", "Valid") }).Increment();
+                System.Console.WriteLine();
 
-                // Calculate the average value of the numbers
-                OkanshiMonitor.AverageGauge("AverageNumber").Set(result);
-
-                // Calculate the minimum value of the numbers
-                OkanshiMonitor.MinGauge("MinimumNumber").Set(result);
-
-                // Calculate the maximum value of the numbers
-                OkanshiMonitor.MaxGauge("MaximumNumber").Set(result);
-                count++;
+                CreateCounter(measurementName, registry).Increment();
             }
+        }
+
+        private static CounterZeroFilter<long> CreateCounter(string measurementName, OkanshiMonitorRegistry registry)
+        {
+            var config = MonitorConfig.Build("Key press").WithTags(OkanshiMonitor.DefaultTags)
+                .WithTags(new[] {new Tag("combination", measurementName)});
+            var monitor = registry.GetOrAdd(config, x => new CounterZeroFilter<long>(new Counter(x)));
+            return monitor;
+        }
+
+	    private static CounterZeroFilter<long> FilterCounter(string name, IEnumerable<Tag> tags)
+	    {
+		    var config = MonitorConfig.Build(name)
+			    .WithTags(OkanshiMonitor.DefaultTags)
+			    .WithTags(tags);
+		    var registry = DefaultMonitorRegistry.Instance;
+		    var monitor = registry.GetOrAdd(config, x => new CounterZeroFilter<long>(new Counter(x)));
+		    return monitor;
+	    }
+
+        class MyObserver : IMetricObserver
+        {
+            public MyObserver(IMetricPoller poller)
+            {
+                poller.RegisterObserver(Update);
+            }
+
+            public async Task Update(IEnumerable<Metric> metrics)
+            {
+                Console.WriteLine("***************************************************************");
+                Console.WriteLine(JsonConvert.SerializeObject(metrics, Formatting.Indented));
+                Console.WriteLine("***************************************************************\n\n");
+            }
+
+            public void Dispose() { }
         }
     }
 }
